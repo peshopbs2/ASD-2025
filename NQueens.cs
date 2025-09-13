fd4da9e1b04e552e41c60e319815c4be72c1cc9b
peshopbs2 <peshopbs2@gmail.com>
2025-09-13 11:35:16 +0300

    Create NQueens.cs

---
+++ NQueens.cs
@@ -0,0 +1,76 @@
+
+class Program
+{
+    public const int N = 30;
+    static bool CanPlace(int[,] board, int row, int col)
+    {
+        for(int i = 0; i < col; i++) //check row
+        {
+            if (board[row, i] == 1)
+            {
+                return false;
+            }
+        }
+
+        //up left diagonal
+        for (int i = row, j = col; i >= 0 && j >= 0; i--, j--)
+        {
+            if (board[i, j] == 1)
+            {
+                return false;
+            }
+        }
+
+        //down left diagonal
+        for (int i = row, j = col; i < N && j >= 0; i++, j--)
+        {
+            if (board[i, j] == 1)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+    static bool Solve(int[,] board, int col)
+    {
+        if(col >= N)
+        {
+            Print(board);
+            return true;
+        }
+
+        for(int i = 0; i < N; i++) //try different rows in the columns
+        {
+            if(CanPlace(board, i, col))
+            {
+                board[i, col] = 1;
+                if(Solve(board, col + 1))
+                {
+                    return true;
+                }
+                board[i, col] = 0;
+            }
+        }
+        return false;
+    }
+
+    private static void Print(int[,] board)
+    {
+        for (int i = 0; i < N; i++)
+        { 
+            for(int j = 0; j < N; j++)
+            {
+                var symbol = (board[i, j] == 1 ? "Q" : ".");
+                Console.Write(symbol);
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static void Main()
+    {
+        int[,] board = new int[N, N];
+        Solve(board, 0);
+    }
+}
