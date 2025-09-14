45360d5412b136ab13fc0dd4a92dcfde34b94068
peshopbs2 <peshopbs2@gmail.com>
2025-09-14 13:22:36 +0300

    Create FibDp.cs

---
+++ FibDp.cs
@@ -0,0 +1,10 @@
+int n = int.Parse(Console.ReadLine());
+int[] dp = new int[n + 1];
+dp[1] = 1;
+dp[2] = 1;
+for (int i = 2; i <= n; i++)
+{
+    dp[i] = dp[i - 1] + dp[i - 2];
+}
+
+Console.WriteLine(dp[n]);
